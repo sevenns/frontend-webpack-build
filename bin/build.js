8b16a3efa8f9f5f31280032d02dcfd2f78ee9451
sevenns <a.sevenns@gmail.com>
2018-05-23 16:08:17 +0300

    getting rid of npm link/bash exec

--- bin/build.js
+++ bin/build.js
@@ -1,11 +1,7 @@
-#! /usr/bin/env node
 const paths = require('../config/paths');
 const rmrf = require('rimraf');
 const webpack = require('webpack');
 const webpackConfig = require('../config/webpack/build');
-
-process.on('SIGINT', process.exit);
-
 const compiler = webpack(webpackConfig);
 
 rmrf(paths.build, error => {
--- bin/dev.js
+++ bin/dev.js
@@ -1,9 +1,6 @@
-#! /usr/bin/env node
 const nodemon = require('nodemon');
 const nodemonConfig = require('../config/nodemon');
 const path = require('path');
 const fs = require('fs');
-
-process.on('SIGINT', process.exit);
 
 nodemon(nodemonConfig).on('quit', process.exit);
