5d20f5c06fc91f4a98373d2271ab713442307bc4
sevenns <a.sevenns@gmail.com>
2018-06-01 12:21:59 +0300

    hotfix for icon component. another minor changes.

--- bin/icons.js
+++ bin/icons.js
@@ -8,18 +8,18 @@ const filename = 'sprites.svg';
 const inputPath = `${paths.client}/icons`;
 const outputPath = `${paths.images}/${filename}`;
 const commands = {
-  svgo: `svgo -f ${paths.client}/icons`,
+  svgo: `svgo -f ${inputPath}`,
   generate: `svg-sprite-generate -d ${inputPath} -o ${outputPath}`
 };
 
 spinner.start(chalk.cyan.bold('Processing icons...\n'));
 
 exec.quiet(commands.svgo).then(() => {
-  spinner.succeed(chalk.green.bold('Processing icons complete...\n'));
+  spinner.succeed(chalk.green.bold('Processing icons complete.\n'));
 
   spinner.start(chalk.cyan.bold('Generating icons sprite...\n'));
 
   exec.quiet(commands.generate).then(() => {
-    spinner.succeed(chalk.green.bold('Generating icons sprite complete...\n'));
+    spinner.succeed(chalk.green.bold('Generating icons sprite complete.\n'));
   });
 });
